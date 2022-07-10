09ccbd60bd930ff90bd38aff08a21a3edd5a3e39
emrt6011 <73031519+emrt6011@users.noreply.github.com>
2022-07-10 03:37:19 +0300

    Add files via upload

---
+++ geolocate.py
@@ -0,0 +1,39 @@
+import geocoder
+import folium
+from art import *
+import time
+import os
+import webbrowser
+import requests
+
+os.system("color b")
+os.system("cls")
+tprint("emirtools")
+
+time.sleep(1)
+
+myip = requests.get('https://icanhazip.com/')
+print(f'my ip addr is --> {myip.text}')
+print("\n")
+xx = input("ip --> ")
+
+ip = geocoder.ip(str(xx))
+
+location = ip.latlng
+
+map = folium.Map(location=location, zoom_start=10)
+folium.CircleMarker(location=location, radius=50, color="red").add_to(map)
+folium.Marker(location).add_to(map)
+
+map
+
+map.save("location.html")
+
+print(ip.city)
+print(ip.latlng)
+
+url = "file://C:/Users/lenovo/Documents/aaa/location.html" # add your location.html file
+webbrowser.open(url,new=2)
+
+time.sleep(2)
+os.remove("C:/Users/lenovo/Documents/aaa/location.html")
